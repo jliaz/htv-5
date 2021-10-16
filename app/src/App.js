85d3e502f77cc6a15499b9c82426538babf8ad45
Zhaohang Yan <10969mart1n@gmail.com>
2021-10-16 15:36:36 +0800

    Adding AddNodeSidebar and Scatterplot

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import DnDFlow from './components/AddNodeSidebar/DnDFlow';
+import PlotGraph from './components/ScatterPlot/PlotGraph'
 
 function App() {
   return (
     <div className="App">
-      
+      {/* <DnDFlow /> */}
+      <PlotGraph />
     </div>
   );
 }
---
+++ app/src/components/AddNodeSidebar/AddNodeSidebar.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import AddNodeSidebar from './AddNodeSidebar';
+
+export default {
+  component: AddNodeSidebar,
+  title: 'Components/AddNodeSidebar',
+}
+
+export const Primary = () => <AddNodeSidebar />;
